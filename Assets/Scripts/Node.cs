bb88181e3587ca517de9f395ff4cec903feee954
Howard van Waard <s5079830@bournemouth.ac.uk>
2020-03-11 17:55:02 +0000

    can spawn roots which sort of grow

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    public Node partner;
+
+    [SerializeField]
+    GameObject connector;
+
+    public GameObject root;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (partner && !root) {
+            //float angle = Vector2.Angle(transform.position, partner.transform.position);
+            Vector2 angle = (transform.position - partner.transform.position);
+            float length = Vector2.Distance(transform.position, partner.transform.position) / 2.0f;
+            Vector2 pos = (transform.position + partner.transform.position) / 2.0f;
+            root = Instantiate(connector, transform.position, Quaternion.identity);
+            partner.root = root;
+            root.transform.localScale = new Vector2(0, 0.3f);
+            root.transform.forward = angle;
+            root.transform.eulerAngles = new Vector3(0, 0, root.transform.eulerAngles.x);
+            Connector con = connector.GetComponent<Connector>();
+            con.maxLength = length;
+            con.startPos = transform.position;
+            con.endPos = pos;
+            con.start = this;
+        }
+    }
+}
---
+++ Assets/Scripts/Roots.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roots : MonoBehaviour {
+    [SerializeField]
+    GameObject Node;
+
+    GameObject parentNode;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0) && !parentNode) {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            parentNode = Instantiate(Node, pos, Quaternion.identity) as GameObject;
+
+        }
+        else if (Input.GetMouseButtonDown(0) && parentNode) {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject childNode = Instantiate(Node, pos, Quaternion.identity) as GameObject;
+            childNode.GetComponent<Node>().partner = parentNode.GetComponent<Node>();
+            parentNode.GetComponent<Node>().partner = childNode.GetComponent<Node>();
+            parentNode = null;
+        }
+
+    }
+}
